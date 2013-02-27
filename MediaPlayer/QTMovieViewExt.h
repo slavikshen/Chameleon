611732851973ff584b405c62cbce2db2fbcc5644
Jerry Tian <tianjing@ihanghai.com>
2013-02-27 20:04:47 +0800

    readd files

---
+++ MediaPlayer/QTMovieViewExt.h
@@ -0,0 +1,13 @@
+//
+//  QTMovieViewExt.h
+//  MediaPlayer
+//
+//  Created by Jerry Tian on 2/26/13.
+//
+//
+
+#import <QTKit/QTKit.h>
+
+@interface QTMovieViewExt : QTMovieView
+
+@end
---
+++ MediaPlayer/QTMovieViewExt.m
@@ -0,0 +1,28 @@
+//
+//  QTMovieViewExt.m
+//  MediaPlayer
+//
+//  Created by Jerry Tian on 2/26/13.
+//
+//
+
+#import "QTMovieViewExt.h"
+
+@implementation QTMovieViewExt
+
+- (id)initWithFrame:(NSRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code here.
+    }
+    
+    return self;
+}
+
+- (void)drawRect:(NSRect)dirtyRect
+{
+    // Drawing code here.
+}
+
+@end
